f49ef312058eef8728feb07d4cee44a6f0d71f40
ShwetaGote-py <shwetagote.work@gmail.com>
2025-03-13 13:14:47 +0530

    Update compare_excel_files.py
    Added exception handlaning and error login

--- compare_excel_files.py
+++ compare_excel_files.py
@@ -1,19 +1,23 @@
 import pandas as pd
+import logging
 
 def compare_excel_files(file1, file2, sheet_name=0):
-    # Read the Excel files
-    df1 = pd.read_excel(file1, sheet_name=sheet_name)
-    df2 = pd.read_excel(file2, sheet_name=sheet_name)
-    #df1 = pd.read_csv(file1)
-    #df2 = pd.read_csv(file2)
+    try:
+        # Read the Excel files
+        df1 = pd.read_excel(file1, sheet_name=sheet_name)
+        df2 = pd.read_excel(file2, sheet_name=sheet_name)
+        #df1 = pd.read_csv(file1)
+        #df2 = pd.read_csv(file2)
 
-    df1, df2 = df1.align(df2, join='outer', axis=0, fill_value=None)
-    df1, df2 = df1.align(df2, join='outer', axis=1, fill_value=None)
+        df1, df2 = df1.align(df2, join='outer', axis=0, fill_value=None)
+        df1, df2 = df1.align(df2, join='outer', axis=1, fill_value=None)
 
-    # Compare the two DataFrames
-    comparison_result = df1.compare(df2)
+        # Compare the two DataFrames
+        comparison_result = df1.compare(df2)
 
-    return comparison_result
+        return comparison_result
+    except Exception as e:
+        logging.exception("An error occurred while comparing the Excel files.")
 
 def save_comparison_result(result, output_file):
     with pd.ExcelWriter(output_file) as writer:
